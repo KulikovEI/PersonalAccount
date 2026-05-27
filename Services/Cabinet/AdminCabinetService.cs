d0467edbb074c8d2930cecb27719960251d8ce04
Evgeniy <eugene020292@gmail.com>
2026-05-27 23:29:35 +0300

    Добавление сервиса для кабинета администратора(IAdminCabinetService)

---
+++ Services/Cabinet/AdminCabinetService.cs
@@ -0,0 +1,22 @@
+﻿using PersonalAccount.Repositories;
+using PersonalAccount.Models;
+
+namespace PersonalAccount.Services.Cabinet
+{
+    public class AdminCabinetService(
+        IAccountRepo accountRepo,
+        IStudentProfileRepo studentProfileRepo) : IAdminCabinetService
+    {
+        public async Task<Dictionary<int, AccountModel>> GetAllStudentAccountsAsync()
+        {
+            var studentAccounts = await accountRepo.GetByRoleAsync(AccountRole.Student);
+
+            return studentAccounts.ToDictionary(account => account.Id);
+        }
+
+        public async Task<List<StudentProfileModel>> GetAllStudentProfilesAsync()
+        {
+            return await studentProfileRepo.GetAllAsync();
+        }
+    }
+}
---
+++ Services/Cabinet/IAdminCabinetService.cs
@@ -0,0 +1,10 @@
+﻿using PersonalAccount.Models;
+
+namespace PersonalAccount.Services.Cabinet
+{
+    public interface IAdminCabinetService
+    {
+        Task<Dictionary<int, AccountModel>> GetAllStudentAccountsAsync();
+        Task<List<StudentProfileModel>> GetAllStudentProfilesAsync();
+    }
+}
