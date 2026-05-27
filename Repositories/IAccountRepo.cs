6ae08e58cf6811f397a75ef829d67b03dbf98d97
Evgeniy <eugene020292@gmail.com>
2026-05-27 23:11:18 +0300

    Добавление методов в IAccountRepo

--- Repositories/IAccountRepo.cs
+++ Repositories/IAccountRepo.cs
@@ -6,5 +6,6 @@ namespace PersonalAccount.Repositories
     {
         public Task<AccountModel?> GetByEmailAsync(string email);
         public Task<AccountModel?> GetByIdAsync(int id);
+        Task<List<AccountModel>> GetByRoleAsync(AccountRole role);
     }
 }
