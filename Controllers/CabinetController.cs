8996ac5759d81d7bafd109aa0d3c2164a199d1a7
Evgeniy <eugene020292@gmail.com>
2026-05-27 22:33:47 +0300

    Добавление роли в Claim и обновление CabinetController

--- Controllers/CabinetController.cs
+++ Controllers/CabinetController.cs
@@ -15,21 +15,56 @@ public class CabinetController(IStudentCabinetService cabinet, IConfirmationToke
     [HttpGet]
     public async Task<IActionResult> Index()
     {
+        if (User.IsInRole(AccountRole.Admin.ToString()))
+        {
+            return RedirectToAction("Admin");
+        }
+
+        if (User.IsInRole(AccountRole.Student.ToString()))
+        {
+            return RedirectToAction("Student");
+        }
+
+        return Forbid();
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Student")] 
+    public async Task<IActionResult> Student()
+    {
         var accountId = User.GetId();
         var accountEmail = User.GetEmail();
-        if (accountId == null || accountEmail == null) return RedirectToAction("Error", "Home");
+
+        if (accountId is null || string.IsNullOrEmpty(accountEmail))
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
         var student = await cabinet.GetByAccountIdAsync(accountId.Value);
-        if (student == null ) return RedirectToAction("Error", "Home");
-        
-        var isEmailConfirmed = await confirmation.HasAnyConfirmedTokenAsync(student.Id);
-        
+        if (student is null)
+        {
+            return RedirectToAction("Error", "Home");
+        }
+
+        var isEmailConfirmed = await confirmation.HasAnyConfirmedTokenAsync(accountId.Value);
+
         return View(new StudentCabinetViewModel
         {
-            Email =  accountEmail,
+            Email = accountEmail,
             FullName = student.FullName,
-            GroupName =  student.GroupName,
+            GroupName = student.GroupName,
             PhotoUrl = student.PhotoUrl?.ToString(),
             IsEmailConfirmed = isEmailConfirmed
         });
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public IActionResult Admin()
+    {
+        var accountEmail = User.GetEmail() ?? "Администратор";
+        ViewData["AdminEmail"] = accountEmail;
+
+        return View();
     }
 }
