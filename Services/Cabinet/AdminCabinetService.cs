dffeb9b528c59a9ea49bdfc76cf2fb31b92feb7d
Evgeniy <eugene020292@gmail.com>
2026-05-28 14:48:22 +0300

    Добавление Task ConfirmStudentEmailAsync(int id) в IAdminCabinetService

--- Services/Cabinet/AdminCabinetService.cs
+++ Services/Cabinet/AdminCabinetService.cs
@@ -1,12 +1,20 @@
-﻿using PersonalAccount.Repositories;
-using PersonalAccount.Models;
+﻿using PersonalAccount.Models;
+using PersonalAccount.Repositories;
+using PersonalAccount.Services.Confirmation;
 
 namespace PersonalAccount.Services.Cabinet
 {
     public class AdminCabinetService(
         IAccountRepo accountRepo,
-        IStudentProfileRepo studentProfileRepo) : IAdminCabinetService
+        IStudentProfileRepo studentProfileRepo, IConfirmationTokenService confirmationTokenService) : IAdminCabinetService
     {
+        public async  Task ConfirmStudentEmailAsync(int id)
+        {
+            string token = await confirmationTokenService.GenerateTokenAsync(id);
+
+            await confirmationTokenService.ValidateTokenAsync(id, token);
+        }
+
         public async Task<Dictionary<int, AccountModel>> GetAllStudentAccountsAsync()
         {
             var studentAccounts = await accountRepo.GetByRoleAsync(AccountRole.Student);
