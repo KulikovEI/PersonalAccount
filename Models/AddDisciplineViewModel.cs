ab5afcaa1c62b51854e8af9dc4cb52c2ddd4315d
Evgeniy <eugene020292@gmail.com>
2026-06-08 23:43:30 +0300

    2.2 Создание AddDisciplineViewModel.cs

---
+++ Models/AddDisciplineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class AddDisciplineViewModel
+    {
+        [Required(ErrorMessage = "Введите название дисциплины")]
+        [StringLength(100, ErrorMessage = "Название дисциплины не должно превышать 100 символов")]
+        [Display(Name = "Название дисциплины")]
+        public string Name { get; set; } = string.Empty;
+    }
+}
