30a248f9f856d35fee0ae46648aea08f06f64b11
Evgeniy <eugene020292@gmail.com>
2026-06-08 22:26:12 +0300

    1.2 Создание AddGroupViewModel.cs

---
+++ Models/AddGroupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class AddGroupViewModel
+    {
+        [Required(ErrorMessage = "Введите название группы")]
+        [StringLength(50, ErrorMessage = "Название группы не должно превышать 50 символов")]
+        [Display(Name = "Название группы")]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(500, ErrorMessage = "Описание не должно превышать 500 символов")]
+        [Display(Name = "Описание группы")]
+        public string Description { get; set; } = string.Empty;
+
+        [Url(ErrorMessage = "Введите корректный URL-адрес (например, https://site.com)")]
+        [Display(Name = "Ссылка на обложку группы")]
+        public string? ImageUrl { get; set; }
+    }
+}
